cefee61305454cb4a07bb8c7da3874a28617b04e
Dunia Alrabadi <mac@Macs-MacBook-Pro.local>
2017-12-19 21:05:45 -0700

    update app.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const login = (email, password, history) => {
   return dispatch => {
-    axios.post('/auth/sign_in', { email, password })
+    axios.post('/api/auth/sign_in', { email, password })
       .then( res => {
         dispatch({ type: 'LOGIN', user: res.data.data, headers: res.headers });
         history.push('/');
@@ -12,7 +12,7 @@ export const login = (email, password, history) => {
 
 export const register = (email, password, passwordConfirmation, history) => {
   return dispatch => {
-    axios.post('/auth', { email, password, passwordConfirmation })
+    axios.post('/api/auth', { email, password, passwordConfirmation })
       .then( res => {
         dispatch({ type: 'LOGIN', user: res.data.data, header: res.headers });
         history.push('/');
@@ -22,7 +22,7 @@ export const register = (email, password, passwordConfirmation, history) => {
 
 export const logout = (history) => {
   return dispatch => {
-    axios.delete('/auth/sign_out')
+    axios.delete('/api/auth/sign_out')
       .then( res => {
         dispatch({ type: 'LOGOUT' });
       });
@@ -33,7 +33,7 @@ export const validateToken = (callBack = () => {}) => {
   return dispatch => {
     dispatch({ type: 'VALIDATE' });
     const headers = axios.defaults.headers.common;
-    axios.get('/auth/validate_token', headers)
+    axios.get('/api/auth/validate_token', headers)
       .then( res => {
         const user = res.data.data;
         dispatch({ type: 'LOGIN', user, headers: res.headers });
